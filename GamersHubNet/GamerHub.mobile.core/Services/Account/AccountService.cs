fa4bbbacb9ebdf7769ceaff937256f5b60025f73
Kasper Cisewski <kaspercisewski@gmail.com>
2019-11-04 22:36:50 +0100

    delete try catch

--- GamersHubNet/GamerHub.mobile.core/Services/Account/AccountService.cs
+++ GamersHubNet/GamerHub.mobile.core/Services/Account/AccountService.cs
@@ -22,27 +22,21 @@ namespace GamerHub.mobile.core.Services.Account
 
         public async Task<bool> LogInUserAsync(string userName, string password)
         {
-            try
+
+            var client = _httpClientFactoryService.GetNotAuthorizedClient();
+
+            var request = new RestRequest("identity/login");
+            request.Method = Method.POST;
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new UserLoginRequest { Email = userName, Password = password });
+
+            var response = await client.ExecuteAsync(request);
+
+            if (response.Success && response.ResponseData is AuthSuccessResponse)
             {
-                var client = _httpClientFactoryService.GetNotAuthorizedClient();
-
-                var request = new RestRequest("identity/login");
-                request.Method = Method.POST;
-                request.RequestFormat = DataFormat.Json;
-                request.AddJsonBody(new UserLoginRequest { Email = userName, Password = password });
-
-                var response = await client.ExecuteAsync(request);
-
-                if (response.Success && response.ResponseData is AuthSuccessResponse)
-                {
-                    var result = response.ResponseData as AuthSuccessResponse;
-                    _globalStateService.UserData.Token = result.Token;
-                    return true;
-                }
-            }
-            catch (Exception e)
-            {
-
+                var result = response.ResponseData as AuthSuccessResponse;
+                _globalStateService.UserData.Token = result.Token;
+                return true;
             }
 
             return false;
