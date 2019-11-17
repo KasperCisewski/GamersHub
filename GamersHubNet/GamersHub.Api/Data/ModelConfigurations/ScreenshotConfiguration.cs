30e0d37c6886f0a149261b025118a1f40f5c3eb6
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2019-11-17 16:03:20 +0100

    Add some domain objects in a fluent api way

---
+++ GamersHubNet/GamersHub.Api/Data/ModelConfigurations/ScreenshotConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using GamersHub.Api.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GamersHub.Api.Data.ModelConfigurations
+{
+    public class ScreenshotConfiguration : IEntityTypeConfiguration<GameImage>
+    {
+        public void Configure(EntityTypeBuilder<GameImage> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.FileName)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(x => x.Data)
+                .IsRequired();
+
+            builder.Property(x => x.Length)
+                .IsRequired();
+
+            builder.Property(x => x.Width)
+                .IsRequired();
+
+            builder.Property(x => x.Height)
+                .IsRequired();
+
+            builder.Property(x => x.ContentType)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
