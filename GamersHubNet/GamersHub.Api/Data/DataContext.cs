59ae644fd6b4dd5d75d67e4e7682add3688741ea
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2019-11-17 16:03:20 +0100

    Add some domain objects in a fluent api way

--- GamersHubNet/GamersHub.Api/Data/DataContext.cs
+++ GamersHubNet/GamersHub.Api/Data/DataContext.cs
@@ -1,6 +1,7 @@
 ﻿using GamersHub.Api.Domain;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace GamersHub.Api.Data
 {
@@ -12,5 +13,14 @@ namespace GamersHub.Api.Data
         }
 
         public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<Screenshot> Screenshots { get; set; }
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<Metatag> Metatags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
     }
 }
---
+++ GamersHubNet/GamersHub.Api/Data/ModelConfigurations/ScreenshotConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using GamersHub.Api.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GamersHub.Api.Data.ModelConfigurations
+{
+    public class ScreenshotConfiguration : IEntityTypeConfiguration<Screenshot>
+    {
+        public void Configure(EntityTypeBuilder<Screenshot> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(x => x.Data)
+                .IsRequired();
+
+            builder.Property(x => x.Length)
+                .IsRequired();
+
+            builder.Property(x => x.Width)
+                .IsRequired();
+
+            builder.Property(x => x.Height)
+                .IsRequired();
+
+            builder.Property(x => x.ContentType)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
