f1db545281831527123bad4278a608ea2c55d931
Kasper Cisewski <kaspercisewski@gmail.com>
2019-11-19 11:42:19 +0100

    add few domain object to create database

--- GamersHubNet/GamersHub.Api/Domain/RefreshToken.cs
+++ GamersHubNet/GamersHub.Api/Domain/RefreshToken.cs
@@ -7,8 +7,6 @@ namespace GamersHub.Api.Domain
 {
     public class RefreshToken
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Token { get; set; }
         public string JwtId { get; set; }
         public DateTime CreationDate { get; set; }
@@ -16,7 +14,6 @@ namespace GamersHub.Api.Domain
         public bool Used { get; set; }
         public bool Invalidated { get; set; }
         public string UserId { get; set; }
-        [ForeignKey(nameof(UserId))]
         public IdentityUser User { get; set; }
     }
 }
