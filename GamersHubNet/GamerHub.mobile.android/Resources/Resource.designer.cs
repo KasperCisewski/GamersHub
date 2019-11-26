cb978e95dad2fb4197bf5d08b7956436fe1b26f4
Kasper Cisewski <kaspercisewski@gmail.com>
2019-11-27 00:16:47 +0100

    delete unused files

--- GamersHubNet/GamerHub.mobile.android/Resources/Resource.designer.cs
+++ GamersHubNet/GamerHub.mobile.android/Resources/Resource.designer.cs
@@ -9559,25 +9559,13 @@ namespace GamerHub.mobile.android
 			public const int background = 2131492864;
 			
 			// aapt resource value: 0x7F0C0001
-			public const int Games = 2131492865;
+			public const int ic_launcher = 2131492865;
 			
 			// aapt resource value: 0x7F0C0002
-			public const int Home = 2131492866;
+			public const int ic_launcher_foreground = 2131492866;
 			
 			// aapt resource value: 0x7F0C0003
-			public const int ic_launcher = 2131492867;
-			
-			// aapt resource value: 0x7F0C0004
-			public const int ic_launcher_foreground = 2131492868;
-			
-			// aapt resource value: 0x7F0C0005
-			public const int ic_launcher_round = 2131492869;
-			
-			// aapt resource value: 0x7F0C0006
-			public const int ProfileIcon = 2131492870;
-			
-			// aapt resource value: 0x7F0C0007
-			public const int Search = 2131492871;
+			public const int ic_launcher_round = 2131492867;
 			
 			static Mipmap()
 			{
