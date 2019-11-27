9a190801426a7b6bff07be03835c54d721826afb
Kasper Cisewski <kaspercisewski@gmail.com>
2019-11-27 13:10:09 +0100

    Add viewmodels

---
+++ GamersHubNet/GamerHub.mobile.core/ViewModels/CoreApp/Games/GamesCategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GamerHub.mobile.core.Models;
+using GamerHub.mobile.core.ViewModels.Base;
+
+namespace GamerHub.mobile.core.ViewModels.CoreApp.Games
+{
+    public partial class GamesCategoryViewModel : BaseViewModel<GameCategoryModel>
+    {
+        public override void Prepare(GameCategoryModel parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GamersHubNet/GamerHub.mobile.core/ViewModels/CoreApp/Games/GamesChosenForUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using GamerHub.mobile.core.ViewModels.Base;
+
+namespace GamerHub.mobile.core.ViewModels.CoreApp.Games
+{
+    public partial class GamesChosenForUserViewModel : BaseViewModel
+    {
+        public GamesChosenForUserViewModel()
+        {
+
+        }
+
+        public override Task Initialize()
+        {
+            return base.Initialize();
+        }
+    }
+}
