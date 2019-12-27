8ec7d5ddb8f79a624cae05dd0f2ba4e33b836a7a
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2019-12-27 11:04:19 +0100

    Add seeder

---
+++ GamersHubNet/GamersHub.DbSeeder/GameCategoryEnumConverter.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using GamersHub.Shared.Data.Enums;
+using System;
+
+namespace GamersHub.DbSeeder
+{
+    public class GameCategoryEnumConverter<T> : EnumConverter where T : struct
+    {
+        public GameCategoryEnumConverter() : base(typeof(T)) { }
+
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            if (!Enum.TryParse(text, out GameCategory gameCategory))
+            {
+                throw new InvalidCastException($"Invalid value to EnumConverter. Type: {typeof(T)} Value: {text}");
+            }
+
+            return gameCategory;
+        }
+    }
+}
