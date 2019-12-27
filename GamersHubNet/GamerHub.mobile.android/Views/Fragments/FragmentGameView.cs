3a8c9bee0edd51de35ea184d3abbbccc1f5d3938
Kasper Cisewski <kaspercisewski@gmail.com>
2019-12-27 20:48:09 +0100

    impove game view

--- GamersHubNet/GamerHub.mobile.android/Views/Fragments/FragmentGameView.cs
+++ GamersHubNet/GamerHub.mobile.android/Views/Fragments/FragmentGameView.cs
@@ -1,9 +1,12 @@
-﻿using Android.OS;
+﻿using System.Collections.Generic;
+using Android.OS;
 using Android.Runtime;
+using Android.Support.V4.View;
 using Android.Views;
 using GamerHub.mobile.android.Views.Fragments.Base;
 using GamerHub.mobile.core.ViewModels;
 using GamerHub.mobile.core.ViewModels.CoreApp.Game;
+using MvvmCross.Droid.Support.V4;
 using MvvmCross.Platforms.Android.Binding.BindingContext;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 
@@ -17,6 +20,34 @@ namespace GamerHub.mobile.android.Views.Fragments
         {
             base.OnCreateView(inflater, container, savedInstanceState);
             var view = this.BindingInflate(Resource.Layout.Fragment_Game_View, null);
+
+            var pager = view.FindViewById<ViewPager>(Resource.Id.game_view_main_with_view_pager);
+
+            if (pager != null)
+            {
+                var fragments = new List<MvxViewPagerFragmentInfo>
+                {
+                    new MvxViewPagerFragmentInfo(
+                        "Screenshots",
+                        "",
+                        typeof(FragmentGameScreenshotsView),
+                        ViewModel.GameScreenshotsViewModel
+                    ),
+                    new MvxViewPagerFragmentInfo(
+                        "Video",
+                        "",
+                        typeof(FragmentGameVideoView),
+                        ViewModel.GameVideoViewModel
+                    ),
+                    new MvxViewPagerFragmentInfo(
+                        "Prices",
+                        "",
+                    typeof(FragmentGamePricesView),
+                    ViewModel.GamePricesViewModel
+                    )
+                };
+                pager.Adapter = new MvxFragmentPagerAdapter(Activity, ChildFragmentManager, fragments);
+            }
 
             return view;
         }
