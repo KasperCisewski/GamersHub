38acc4e75b84d8e8c3938bbeec62b1c708ec2f48
Kasper Cisewski <kaspercisewski@gmail.com>
2019-12-30 11:38:07 +0100

    add profile service and add endpoint values

---
+++ GamersHubNet/GamerHub.Shared/Api/ApiRoutes.cs
@@ -0,0 +1,59 @@
+﻿namespace GamersHub.Shared.Api
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+
+        public static class Identity
+        {
+            public const string Login = Root + "/identity/login";
+
+            public const string Register = Root + "/identity/register";
+
+            public const string Refresh = Root + "/identity/refresh";
+
+            public const string UserWithEmailExists = Root + "/identity/userWithEmailExists";
+
+            public const string UserWithUsernameExists = Root + "/identity/userWithUsernameExists";
+        }
+
+        public static class Search
+        {
+            public const string SearchGames = Root + "/search/searchgames";
+        }
+
+        public static class GameImages
+        {
+            public const string AddGameImage = Root + "/images/add";
+        }
+
+        public static class Games
+        {
+            public const string GetGamesForHomeScreen = Root + "/games/getGamesForHomeScreen";
+            public const string AddGameToWishList = Root + "/games/addGameToWishList";
+            public const string AddGameToVault = Root + "/games/addGameToVault";
+            public const string GetScreenshotsForGame = Root + "/games/GetScreenShotsForGame";
+            public const string GetPricesForGame = Root + "/games/getPricesForGame";
+            public const string GetVideoUrl = Root + "/games/getVideoUrl";
+            public const string GetGamesByCategory = Root + "/games/getGamesByCategory";
+        }
+
+        public static class Profile
+        {
+            public const string GetUserProfileInformation = Root + "/profile/getUserProfileInformation";
+            public const string GetUserFriends = Root + "/profile/getUserFriends";
+            public const string SearchUsers = Root + "/profile/searchUsers";
+            public const string GetGamesInVault = Root + "/profile/getGamesInVault";
+            public const string GetWishListGames = Root + "/profile/getWishListGames";
+            //TODO Marcel ma to zrobić, jakiś jego model stworzyć obrazek
+            public const string GetHeatMap = Root + "/profile/getHeatMap";
+        }
+
+        public static class Test
+        {
+            public const string Get = Root + "/test";
+
+            public const string GetAuth = Root + "/test/getAuth";
+        }
+    }
+}
