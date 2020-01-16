0f2ca50c6f7fb3f9a6a32e63b81031f714c1d792
Kasper Cisewski <kaspercisewski@gmail.com>
2020-01-17 00:15:46 +0100

    imrpove layout and add layour dform games for user

---
+++ GamersHubNet/GamerHub.mobile.android/Views/Fragments/FragmentGamesChosenForUser.cs
@@ -0,0 +1,32 @@
+﻿using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Views;
+using GamerHub.mobile.android.Views.Fragments.Base;
+using GamerHub.mobile.core.ViewModels;
+using GamerHub.mobile.core.ViewModels.CoreApp.Games;
+using MvvmCross;
+using MvvmCross.Droid.Support.V7.RecyclerView;
+using MvvmCross.Platforms.Android;
+using MvvmCross.Platforms.Android.Binding.BindingContext;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+
+namespace GamerHub.mobile.android.Views.Fragments
+{
+    [MvxFragmentPresentation(typeof(MainViewModel), Resource.Id.main_frame_layout, true)]
+    [Register("GamerHub.mobile.Android.Views.Fragments.FragmentGamesChosenForUser")]
+    public class FragmentGamesChosenForUser : FragmentBase<GamesChosenForUserViewModel>
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+            var view = this.BindingInflate(Resource.Layout.Fragment_Games_Chosen_For_User, null);
+
+            var recyclerView = view.FindViewById<MvxRecyclerView>(Resource.Id.games_chosen_for_user_recycler_view);
+
+            var layoutManager = new GridLayoutManager(Mvx.IoCProvider.Resolve<IMvxAndroidCurrentTopActivity>().Activity, 2);
+            recyclerView.SetLayoutManager(layoutManager);
+            return view;
+        }
+    }
+}
